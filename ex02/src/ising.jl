8e31383c0a9aefc1e35ed58425a98992654992c1
emhl <emil.steiner@uni-konstanz.de>
2024-04-29 13:32:44 +0200

    copy ising.jl from ex01

---
+++ ex02/src/ising.jl
@@ -0,0 +1,199 @@
+using Statistics
+
+function create_grid(N1::Int, N2::Int, N3::Int)
+    return rand([-1, 1], N1, N2, N3)
+end
+
+
+@doc "Energy calculation for different input arguments"
+function energy(grid::Array{Int,3}, J::Float64, B::Float64=0.0)
+    N1, N2, N3 = size(grid)
+    E = 0.0
+    for i in 1:N1
+        for j in 1:N2
+            for k in 1:N3
+                E += -J * grid[i, j, k] * (
+                         grid[mod1(i + 1, N1), j, k]
+                         + grid[i, mod1(j + 1, N2), k]
+                         + grid[i, j, mod1(k + 1, N3)]
+                     )
+            end
+        end
+    end
+    E -= B * sum(grid)
+    return E
+end
+
+# create function that only calculates the energy diference that the single flip would cause
+function energy_diff(grid::Array{Int,3}, flip_position::Tuple{Int,Int,Int}; J::Float64=1.0, B::Float64=0.0)
+    N1, N2, N3 = size(grid)
+    delta_E = 0.0
+    i, j, k = flip_position
+    proposed_spin = -grid[i, j, k]
+    # 2x because the Energy contribution is symmetric
+    delta_E += -J * 2 * proposed_spin * (
+                   grid[mod1(i + 1, N1), j, k]
+                   + grid[i, mod1(j + 1, N2), k]
+                   + grid[i, j, mod1(k + 1, N3)]
+                   + grid[mod1(i - 1, N1), j, k]
+                   + grid[i, mod1(j - 1, N2), k]
+                   + grid[i, j, mod1(k - 1, N3)]
+               )
+    delta_E -= B * 2 * proposed_spin
+    return delta_E
+end
+
+@doc "Creates a lookup table for the values of the exponential function."
+function create_lookup_table(T::Float64; J::Float64=1.0, dimension::Int=3)
+    lookup_table = Dict{Float64,Float64}()
+    for e in -4*dimension:4*dimension
+        lookup_table[J*e] = state_probability(J * e, T)
+    end
+    return lookup_table
+end
+
+@doc "Magnetisation of a given grid"
+function magnetisation(grid::Array{Int,3})
+    return sum(grid) / length(grid)
+end
+
+@doc "Calculates the probabilites for a given temperature and Energy for the lookup table."
+function state_probability(E::Float64, T::Float64)
+    return exp(-E / T)
+end
+
+@doc "Calculates the weighted mean value of an Array"
+function mean_observable(values::Array{Float64,1}, weights::Array{Float64,1})
+    #TODO can this function be vectorized?
+    # return sum(values .* weights) / sum(weights)
+    s = 0.0
+    for (value, weight) in zip(values, weights)
+        s += value * weight
+    end
+    return s / sum(weights) # normalize
+end
+
+@doc "Calculates the weighted mean of the squared array values"
+function mean_observable_squared(values::Array{Float64,1}, weights::Array{Float64,1})
+    #TODO can this function be vectorized?
+    # return sum((values .^ 2) .* weights) / sum(weights)
+    s = 0.0
+    for (value, weight) in zip(values, weights)
+        s += value^2 * weight
+    end
+    return s / sum(weights) # normalize
+end
+
+@doc "Calculates the magnetic suscebtibility using the variance of the magnetization"
+function magnetic_suceptibility(magnetisation_values::Array{Float64,1}, weights::Array{Float64,1})
+    return mean_observable_squared(magnetisation_values, weights) - mean_observable(magnetisation_values, weights)^2
+end
+
+@doc "Calculates the heat capacity using the variance of the Energy"
+function heat_capacity(energy_values::Array{Float64,1}, weights::Array{Float64,1}, T::Float64)
+    return (mean_observable_squared(energy_values, weights) - mean_observable(energy_values, weights)^2) / T^2
+end
+
+
+@doc "Metropolitan Step function for different input arguments"
+function metropolis_step(grid::Array{Int,3}, J::Float64, lookup_table::Dict{Float64,Float64}, T::Float64=0.0, B::Float64=0.0)
+    N1, N2, N3 = size(grid)
+    i = rand(1:N1)
+    j = rand(1:N2)
+    k = rand(1:N3)
+    dE = energy_diff(grid, (i, j, k), J=J, B=B)
+    if rand() < lookup_table[dE]
+        grid[i, j, k] *= -1
+        dM = 2 * grid[i, j, k]
+    else
+        dE = 0
+        dM = 0
+    end
+    return grid, dE, dM
+end
+
+
+
+@doc "Function for n repetitions of metropolis_step, tracking the energy and magnetization for constant temperature and field"
+function monte_carlo_const_temp(grid::Array{Int,3}, J::Float64, T::Float64, B::Float64, n::Int64)
+
+    energies, magnetisations = Float64[], Float64[]
+    lookup_table = create_lookup_table(T, J=J)
+    for i in 1:n
+        grid, _ = metropolis_step(grid, J, lookup_table, T, B)
+        push!(energies, energy(grid, J, B))
+        push!(magnetisations, magnetisation(grid))
+    end
+
+    return grid, energies, magnetisations
+
+end
+
+@doc "Function to create an equilibrated grid after N Thermalisation steps"
+function create_equilibrated_grid(; grid_size::Int=10, J::Float64=1.0, lookup_table::Dict{Float64,Float64}, T::Float64=0.0, B::Float64=0.0, N::Int=100_000)
+    grid = create_grid(grid_size, grid_size, grid_size) # always start with a new random grid
+    for i in 1:N
+        grid, _ = metropolis_step(grid, J, lookup_table, T, B)
+    end
+    return grid
+end
+
+@doc "function for sweeping over a temperature intervall using T_Steps steps"
+function temp_sweep(; grid_size::Int=10, J::Float64=1.0, T_Start::Float64=0.0, T_End::Float64=10.0, B::Float64=0.0, T_Steps::Int=100, N_Sample::Int=1000, N_Thermalize::Int=100_000, use_same_grid::Bool=false)
+    energies, energies_sq, magnetisations, magnetisations_sq, temps = Float64[], Float64[], Float64[], Float64[], Float64[]
+    if (use_same_grid)
+        # on first step the grid gets thermalized twice as long
+        grid = create_equilibrated_grid(grid_size=grid_size, J=J, lookup_table=create_lookup_table(T_Start, J=J), T=T_Start, B=B, N=N_Thermalize)
+    end
+    for T in range(T_Start, T_End, T_Steps)
+        energies_, magnetisations_ = Float64[], Float64[]
+        lookup_table = create_lookup_table(T, J=J)
+        if (!use_same_grid) # default behaviour
+            grid = create_equilibrated_grid(grid_size=grid_size, J=J, lookup_table=lookup_table, T=T, B=B, N=N_Thermalize)
+        else
+            for i in 1:N_Thermalize
+                grid, _ = metropolis_step(grid, J, lookup_table, T, B)
+            end
+        end
+        len_grid = length(grid)
+        energy_ = energy(grid, J, B)
+        magnetisation_ = magnetisation(grid)
+        for i in 1:N_Sample
+            grid, dE, dM = metropolis_step(grid, J, lookup_table, T, B)
+            energy_ += dE
+            magnetisation_ += dM / len_grid
+            push!(energies_, energy_)
+            push!(magnetisations_, magnetisation_)
+        end
+        push!(magnetisations, mean(magnetisations_))
+        push!(magnetisations_sq, mean(magnetisations_ .^ 2))
+        push!(energies, mean(energies_))
+        push!(energies_sq, mean(energies_ .^ 2))
+        push!(temps, T)
+    end
+    return (energies, energies_sq), (magnetisations, magnetisations_sq), temps
+end
+
+
+@doc "derivative of one vector by the other"
+function dv(x::Vector, y::Vector)
+    if length(y) > length(x)
+        laenge = length(x)
+    else
+        laenge = length(y)
+    end
+
+    derivative = Float64[]
+
+    for i in 1:laenge
+        if i == 1
+            push!(derivative, (y[i+1] - y[i]) / (x[i+1] - x[i]))
+        elseif i == laenge
+            push!(derivative, (y[i] - y[i-1]) / (x[i] - x[i-1]))
+        else
+            push!(derivative, (y[i+1] - y[i-1]) / (x[i+1] - x[i-1]))
+        end
+    end
+
+    return derivative
+end
